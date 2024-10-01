24703bc5c8602d7af6c2d54520fab41aad080557
Teleejr <81259920+Teleejr@users.noreply.github.com>
2024-10-01 17:52:06 -0400

    Added a class for game pieces

---
+++ src/main/java/org/example/pieces/GamePiece.java
@@ -0,0 +1,10 @@
+package org.example.pieces;
+
+public class GamePiece {
+
+    private String rock;
+    private String paper;
+    private String scissors;
+    private String Xs;
+    private String Os;
+}
