d819966181efb20bd4480dd618709d46befa1a12
Teleejr <81259920+Teleejr@users.noreply.github.com>
2024-10-02 18:34:10 -0400

    Added steps for TicTacToe class and created GamePieces class

--- src/main/java/org/example/games/TicTacToe.java
+++ src/main/java/org/example/games/TicTacToe.java
@@ -1,15 +1,66 @@
 package org.example.games;
 
+import org.example.pieces.GamePiece;
+
+import java.util.Arrays;
+
 public class TicTacToe {
 
-    private String board = "   ||   ||   \n" +
-            "=============\n" +
-            "   ||   ||   \n" +
-            "=============\n" +
-            "   ||   ||   \n";
+    private String[] board;
+    private GamePiece turn;
+
+    //Step 1: Players choose X or O
+    //Step 2: Determine win conditions
+    public String winner() {
+        String line = "";
+        boolean xWins = false;
+        boolean oWins = false;
+
+        for(int i = 0; i < 8; i++) {
+            switch(i) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+
+            if(line.equals("XXX")) {
+                xWins = true;
+                return "X wins!";
+            }
+            if(line.equals("OOO")) {
+                oWins = true;
+                return "O wins!";
+            }
+        }
+        return "It's a draw!";
+    }
+    //Step 3: Players alternately place their pieces
+    //Step 4: Display board after each move
+    //Step 5: A player wins or draws
 
     @Override
     public String toString() {
-        return "TicTacToe!\n" + board;
+        return "TicTacToe! \n" + board;
     }
 }
--- src/main/java/org/example/pieces/GamePiece.java
+++ src/main/java/org/example/pieces/GamePiece.java
@@ -7,4 +7,5 @@ public class GamePiece {
     private String scissors;
     private String Xs;
     private String Os;
+
 }
